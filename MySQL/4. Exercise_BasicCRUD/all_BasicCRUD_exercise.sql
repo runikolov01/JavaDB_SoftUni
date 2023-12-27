df9c9763a4e4d234aa3f207169153cfde60c5efd
Rumen Nikolov <rumenhco2001@abv.bg>
2023-12-27 16:49:05 +0200

    ex. 14

--- MySQL/4. Exercise_BasicCRUD/all_BasicCRUD_exercise.sql
+++ MySQL/4. Exercise_BasicCRUD/all_BasicCRUD_exercise.sql
@@ -63,3 +63,11 @@ SELECT first_name, last_name
 FROM employees
 WHERE department_id != 4;
 
+-- 14. Sort Employees Table 
+SELECT * FROM employees
+ORDER BY 
+	salary DESC, 
+	first_name ASC, 
+    last_name DESC,
+    middle_name ASC
+    
