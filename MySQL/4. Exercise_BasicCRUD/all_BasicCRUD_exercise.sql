5b28141fc10aff7bbd123799fba4eb3dde9f8e86
Rumen Nikolov <rumenhco2001@abv.bg>
2023-12-27 17:23:34 +0200

    22. Biggest Countries by Population

--- MySQL/4. Exercise_BasicCRUD/all_BasicCRUD_exercise.sql
+++ MySQL/4. Exercise_BasicCRUD/all_BasicCRUD_exercise.sql
@@ -115,3 +115,12 @@ SELECT * FROM peaks;
 
 SELECT peak_name FROM peaks
 ORDER BY peak_name ASC;
+
+-- 22. Biggest Countries by Population
+SELECT country_name, population 
+FROM countries
+WHERE continent_code = 'EU'
+ORDER BY 
+	population DESC,
+	country_name ASC
+LIMIT 30;
