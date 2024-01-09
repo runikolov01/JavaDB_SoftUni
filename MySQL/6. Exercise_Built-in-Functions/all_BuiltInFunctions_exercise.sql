f61d2e78b3c3a0cde7768efc233344687c480d80
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-09 21:39:01 +0200

    02. Find Names of All Employees by Last Name

--- MySQL/6. Exercise_Built-in-Functions/all_BuiltInFunctions_exercise.sql
+++ MySQL/6. Exercise_Built-in-Functions/all_BuiltInFunctions_exercise.sql
@@ -1,3 +1,8 @@
 -- 01. Find Names of All Employees by First Name
 SELECT first_name, last_name FROM employees
 WHERE first_name REGEXP '^Sa';
+
+-- 02. Find Names of All Employees by Last Name
+SELECT first_name, last_name FROM employees
+WHERE last_name REGEXP 'ei%'
+ORDER BY employee_id;
