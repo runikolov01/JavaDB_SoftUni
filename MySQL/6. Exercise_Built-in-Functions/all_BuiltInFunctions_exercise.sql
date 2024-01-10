33a4adfb3081eb574ba67c6e1c8e090353601728
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-10 16:42:46 +0200

    exercises 11 - 16

--- MySQL/6. Exercise_Built-in-Functions/all_BuiltInFunctions_exercise.sql
+++ MySQL/6. Exercise_Built-in-Functions/all_BuiltInFunctions_exercise.sql
@@ -59,3 +59,65 @@ SELECT country_name, iso_code
 FROM countries
 WHERE country_name REGEXP '(a.*){3}'
 ORDER BY iso_code;
+
+-- 11. Mix of Peak and River Names
+SELECT 
+	p.peak_name, 
+	r.river_name,
+	LOWER(CONCAT(LEFT(p.peak_name, LENGTH(p.peak_name) - 1), r.river_name)) AS mix
+FROM 
+	rivers as r, 
+	peaks as p
+WHERE 
+	UPPER(RIGHT(p.peak_name, 1)) = UPPER(LEFT(r.river_name, 1))
+ORDER BY mix;
+
+-- 12. Games From 2011 and 2012 Year
+-- USE diablo
+SELECT `name`, DATE_FORMAT(`start`, '%Y-%m-%d') AS `start`
+FROM `games`
+WHERE YEAR(`start`) IN (2011, 2012)
+ORDER BY `start`, `name`
+LIMIT 50;
+
+-- 13. User Email Providers
+SELECT 
+	user_name,
+	-- SUBSTRING_INDEX(email, '@', -1) AS `email provider`
+	REGEXP_REPLACE(email, '.*@', '') AS `email provider`
+FROM 
+	users
+ORDER BY
+	`email provider`, user_name;
+
+-- 14. Get Users with IP Address Like Pattern
+SELECT user_name, ip_address
+FROM users
+WHERE ip_address LIKE '___.1%.%.___'
+ORDER BY user_name;
+
+-- 15. Show All Games with Duration
+SELECT 
+	name AS games,
+	CASE 
+	WHEN HOUR(start) BETWEEN 0 AND 11 THEN 'Morning'
+	WHEN HOUR(start) BETWEEN 12 AND 17 THEN 'Afternoon'
+	ELSE 'Evening' 
+	END AS 'Part of the day',
+
+	CASE
+	WHEN duration <= 3 THEN 'Extra Short'
+	WHEN duration BETWEEN 4 AND 6 THEN 'Short'
+	WHEN duration BETWEEN 7 AND 10  THEN 'Long'
+	ELSE 'Extra Long'
+	END AS 'Duration'
+FROM games
+ORDER BY name;
+
+-- 16. Orders Table
+SELECT 
+	product_name, 
+	order_date, 
+	DATE_ADD(order_date, INTERVAL 3 DAY) AS pay_due, 
+	DATE_ADD(order_date, INTERVAL 1 MONTH) AS deliver_due
+FROM orders;
