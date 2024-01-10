513529fb24642edb0a611e4097e108781c0f0384
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-10 16:54:08 +0200

    03. Longest Magic Wand per Deposit Groups

--- MySQL/8. Exercise_Data Aggregation/Data Aggregation - Exercise.sql
+++ MySQL/8. Exercise_Data Aggregation/Data Aggregation - Exercise.sql
@@ -5,3 +5,9 @@ FROM `wizzard_deposits`;
 -- 02. Longest Magic Wand
 SELECT MAX(magic_wand_size) AS `longest_magic_wand`
 FROM `wizzard_deposits`;
+
+-- 03. Longest Magic Wand per Deposit Groups
+SELECT deposit_group, MAX(magic_wand_size) AS `longest_magic_wand`
+FROM `wizzard_deposits`
+GROUP BY deposit_group
+ORDER BY longest_magic_wand ASC, deposit_group ASC;
