5c84d008dfa53797d864b5f218aaf6418bd8b7a5
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-10 16:50:37 +0200

    02. Longest Magic Wand

--- MySQL/8. Exercise_Data Aggregation/Data Aggregation - Exercise.sql
+++ MySQL/8. Exercise_Data Aggregation/Data Aggregation - Exercise.sql
@@ -1,3 +1,7 @@
 -- 01. Records’ Count
 SELECT COUNT(*) AS `count` 
 FROM `wizzard_deposits`;
+
+-- 02. Longest Magic Wand
+SELECT MAX(magic_wand_size) AS `longest_magic_wand`
+FROM `wizzard_deposits`;
