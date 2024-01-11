c084d6c02b7b20519cfbe45613a3aca654e3c47d
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-11 22:48:44 +0200

    17. Salary Challenge

--- MySQL/8. Exercise_Data Aggregation/Data Aggregation - Exercise.sql
+++ MySQL/8. Exercise_Data Aggregation/Data Aggregation - Exercise.sql
@@ -166,6 +166,22 @@ FROM
 WHERE
     manager_id IS NULL;
     
+-- 17. Salary Challenge
+SELECT 
+    `first_name`, `last_name`, `department_id`
+FROM
+    `employees` e1
+WHERE
+    `salary` > (SELECT 
+    -- Намиране на средната заплата на отдела на служител e1:
+            AVG(`salary`)
+        FROM
+            `employees` e2
+        WHERE
+            e1.department_id = e2.department_id)
+ORDER BY `department_id` , `employee_id`
+LIMIT 10;
+    
 -- 18. Departments Total Salaries
 SELECT 
     `department_id`, SUM(`salary`) AS `total_salary`
