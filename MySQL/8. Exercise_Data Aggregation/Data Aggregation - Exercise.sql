123390912c5e8291f6bb001fb93957a5aecf18b9
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-11 21:28:29 +0200

    08. Deposit Charge

--- MySQL/8. Exercise_Data Aggregation/Data Aggregation - Exercise.sql
+++ MySQL/8. Exercise_Data Aggregation/Data Aggregation - Exercise.sql
@@ -32,9 +32,18 @@ GROUP BY deposit_group
 ORDER BY deposit_group;
 
 SELECT *
-FROM wizzard_deposits
+FROM wizzard_deposits;
 
 -- 07. Deposits Filter
 SELECT deposit_group, SUM(deposit_amount) AS `total_sum`
 FROM wizzard_deposits
+WHERE magic_wand_creator = 'Ollivander family'
+GROUP BY deposit_group
+HAVING total_sum < 150000
+ORDER BY total_sum DESC;
 
+-- 08. Deposit Charge
+SELECT `deposit_group`, `magic_wand_creator`, MIN(`deposit_charge`) AS `min_deposit_charge`
+FROM wizzard_deposits
+GROUP BY `magic_wand_creator`, `deposit_group`
+ORDER BY `magic_wand_creator`, `deposit_group`;
