1e9c56c597de6058968790f05cc51e2d2cdf4021
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-11 22:42:36 +0200

    18. Departments Total Salaries

--- MySQL/8. Exercise_Data Aggregation/Data Aggregation - Exercise.sql
+++ MySQL/8. Exercise_Data Aggregation/Data Aggregation - Exercise.sql
@@ -165,3 +165,11 @@ FROM
     `employees`
 WHERE
     manager_id IS NULL;
+    
+-- 18. Departments Total Salaries
+SELECT 
+    `department_id`, SUM(`salary`) AS `total_salary`
+FROM
+    `employees`
+GROUP BY `department_id`
+ORDER BY `department_id`;
