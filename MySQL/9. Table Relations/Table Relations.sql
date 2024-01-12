4d87168ec768229d8ce54e599e6bbca77b7b25cd
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-12 19:22:00 +0200

    3. SoftUni Hiking

--- MySQL/9. Table Relations/Table Relations.sql
+++ MySQL/9. Table Relations/Table Relations.sql
@@ -44,10 +44,15 @@ FROM
     
 -- 3. SoftUni Hiking
 USE camp;
-SELECT starting_point AS 'route_starting_point', end_point AS 'route_ending_point', leader_id
-FROM routes
-
-;
+SELECT 
+    starting_point AS 'route_starting_point',
+    end_point AS 'route_ending_point',
+    leader_id,
+    CONCAT(first_name, ' ', last_name) AS 'leader_name'
+FROM
+    routes
+        JOIN
+    campers ON routes.leader_id = campers.id;
 
 SELECT * FROM campers;
 SELECT * FROM routes;
