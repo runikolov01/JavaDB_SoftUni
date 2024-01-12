996ed868781dfe0d5972d9204b9fd92071e9290e
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-12 21:53:40 +0200

    02. One-To-Many Relationship

--- MySQL/10. Exercise Table Relations/TableRelationsExercise.sql
+++ MySQL/10. Exercise Table Relations/TableRelationsExercise.sql
@@ -33,3 +33,26 @@ SELECT
     *
 FROM
     people;
+    
+-- 02. One-To-Many Relationship
+CREATE TABLE manufacturers (
+    manufacturer_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    established_on DATETIME NOT NULL
+);
+
+CREATE TABLE models (
+    model_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    manufacturer_id INT,
+    CONSTRAINT fk_model_manufacturers FOREIGN KEY (manufacturer_id)
+        REFERENCES manufacturers (manufacturer_id)
+);
+
+ALTER TABLE models AUTO_INCREMENT = 101;
+
+INSERT INTO manufacturers(name, established_on)
+VALUES('BMW', '1916-03-01'), ('Tesla', '2003-01-01'), ('Lada', '1966-05-01');
+
+INSERT INTO models(name, manufacturer_id)
+VALUES ('X1', 1), ('i6', 1), ('Model S', 2), ('Model X', 2),  ('Model 3', 2), ('Nova', 3);
