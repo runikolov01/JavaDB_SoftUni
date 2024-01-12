070f32e66f0018f2ceb51ec537d8da31ea58b0cf
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-12 18:57:04 +0200

    5. Project Management DB*

--- MySQL/9. Table Relations/Table Relations.sql
+++ MySQL/9. Table Relations/Table Relations.sql
@@ -43,7 +43,14 @@ FROM
     campers ON driver_id = campers.id;
     
 -- 3. SoftUni Hiking
+USE camp;
+SELECT starting_point AS 'route_starting_point', end_point AS 'route_ending_point', leader_id
+FROM routes
 
+;
+
+SELECT * FROM campers;
+SELECT * FROM routes;
 
 -- 4. Delete Mountains
 USE mount_peaks;
@@ -64,3 +71,32 @@ CREATE TABLE peaks (
         REFERENCES mountains (id)
         ON DELETE CASCADE
 );
+
+
+-- 5. Project Management DB*
+CREATE TABLE clients (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    client_name VARCHAR(100)
+);
+
+CREATE TABLE projects (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    client_id INT,
+    project_lead_id INT,
+    CONSTRAINT fk_projects_client_id_clients_id FOREIGN KEY (client_id)
+        REFERENCES clients (id)
+);
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    project_id INT,
+    CONSTRAINT fk_employees_project_id_projects_id FOREIGN KEY (project_id)
+        REFERENCES projects (id)
+);
+
+ALTER TABLE projects
+ADD CONSTRAINT fk_projects_project_lead_id_employees_id
+FOREIGN KEY(project_lead_id)
+REFERENCES employees(id);
