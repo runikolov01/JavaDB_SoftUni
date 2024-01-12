c21b87b3b07091ad264966b461adc3c683721ef4
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-12 18:27:29 +0200

    4. Delete Mountains

--- MySQL/9. Table Relations/Table Relations.sql
+++ MySQL/9. Table Relations/Table Relations.sql
@@ -41,3 +41,26 @@ FROM
     vehicles
         JOIN
     campers ON driver_id = campers.id;
+    
+-- 3. SoftUni Hiking
+
+
+-- 4. Delete Mountains
+USE mount_peaks;
+DROP TABLE peaks;
+DROP TABLE mountains;
+
+CREATE TABLE mountains (
+    id INT AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_mountains_id PRIMARY KEY (id)
+);
+
+CREATE TABLE peaks (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `name` VARCHAR(100) NOT NULL,
+    mountain_id INT NOT NULL,
+    CONSTRAINT fk_peaks_mountain_id_mountains_id FOREIGN KEY (mountain_id)
+        REFERENCES mountains (id)
+        ON DELETE CASCADE
+);
