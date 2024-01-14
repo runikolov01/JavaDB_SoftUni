07da9b424e79fb50223c6ff7408626c7650f73cf
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-14 11:56:39 +0200

    03. Many-To-Many Relationship

--- MySQL/10. Exercise Table Relations/TableRelationsExercise.sql
+++ MySQL/10. Exercise Table Relations/TableRelationsExercise.sql
@@ -56,3 +56,47 @@ VALUES('BMW', '1916-03-01'), ('Tesla', '2003-01-01'), ('Lada', '1966-05-01');
 
 INSERT INTO models(name, manufacturer_id)
 VALUES ('X1', 1), ('i6', 1), ('Model S', 2), ('Model X', 2),  ('Model 3', 2), ('Nova', 3);
+
+-- 3. Many-To-Many Relationship
+CREATE TABLE students (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE exams (
+    exam_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+)  AUTO_INCREMENT=101;
+
+
+CREATE TABLE students_exams (
+    student_id INT,
+    exam_id INT,
+    CONSTRAINT pk_students_exams PRIMARY KEY (student_id , exam_id),
+    CONSTRAINT fk_students_exams FOREIGN KEY (exam_id)
+        REFERENCES exams (exam_id),
+    CONSTRAINT fk_exams_students FOREIGN KEY (student_id)
+        REFERENCES students (student_id)
+);
+
+INSERT INTO students(name)
+VALUES('Mila'), ('Toni'), ('Ron');
+
+INSERT INTO exams(name)
+VALUE('Spring MVC'), ('Neo4j'), ('Oracle 11g');
+
+INSERT INTO students_exams
+VALUES(1, 101), (1, 102), (2, 101), (3, 103), (2, 102), (2, 103);
+
+SELECT 
+    *
+FROM
+    students;
+SELECT 
+    *
+FROM
+    exams;
+SELECT 
+    *
+FROM
+    students_exams;
