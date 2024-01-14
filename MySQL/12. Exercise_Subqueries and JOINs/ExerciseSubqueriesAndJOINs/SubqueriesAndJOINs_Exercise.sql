921f46956c29631c0188753f99567307ac618cfc
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-14 19:52:26 +0200

    05. Employees Without Project

--- MySQL/12. Exercise_Subqueries and JOINs/ExerciseSubqueriesAndJOINs/SubqueriesAndJOINs_Exercise.sql
+++ MySQL/12. Exercise_Subqueries and JOINs/ExerciseSubqueriesAndJOINs/SubqueriesAndJOINs_Exercise.sql
@@ -29,3 +29,11 @@ FROM employees AS e
 WHERE e.salary > 15000
 ORDER BY d.department_id DESC
 LIMIT 5;
+
+-- 05. Employees Without Project
+SELECT e.employee_id, e.first_name
+FROM employees AS e
+         LEFT OUTER JOIN employees_projects AS ep ON e.employee_id = ep.employee_id
+WHERE ep.project_id IS NULL
+ORDER BY e.employee_id DESC
+LIMIT 3;
