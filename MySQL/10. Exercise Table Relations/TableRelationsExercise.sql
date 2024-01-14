a35c52e122f372b4773f0e84a17da0c4926f9a4d
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-14 14:50:12 +0200

    05. Online Store Database

--- MySQL/10. Exercise Table Relations/TableRelationsExercise.sql
+++ MySQL/10. Exercise Table Relations/TableRelationsExercise.sql
@@ -117,6 +117,54 @@ REFERENCES teachers(teacher_id)
 ON DELETE NO ACTION;
 SET FOREIGN_KEY_CHECKS=0;
 
+-- 5. Online Store Database
+CREATE DATABASE onlineStore;
+USE onlineStore;
+
+CREATE TABLE cities (
+    city_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+
+CREATE TABLE customers (
+    customer_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    birthday DATE,
+    city_id INT(11),
+    CONSTRAINT fk_customers_cities FOREIGN KEY (city_id)
+        REFERENCES cities (city_id)
+);
+
+CREATE TABLE orders (
+    order_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT(11),
+    CONSTRAINT fk_orders_customers FOREIGN KEY (customer_id)
+        REFERENCES customers (customer_id)
+);
+
+CREATE TABLE item_types (
+    item_type_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+
+CREATE TABLE items (
+    item_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    item_type_id INT(11),
+    CONSTRAINT fk_items_types FOREIGN KEY (item_type_id)
+        REFERENCES item_types (item_type_id)
+);
+
+CREATE TABLE order_items (
+    order_id INT(11),
+    item_id INT(11),
+    CONSTRAINT pk_order_items PRIMARY KEY (order_id , item_id),
+    CONSTRAINT fk_items_orders FOREIGN KEY (order_id)
+        REFERENCES orders (order_id),
+    CONSTRAINT fk_items_id FOREIGN KEY (item_id)
+        REFERENCES items (item_id)
+);
+
 -- 06. University Database
 CREATE TABLE subjects (
     subject_id INT(11) PRIMARY KEY AUTO_INCREMENT,
