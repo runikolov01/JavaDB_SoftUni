ec52c6c154abb384c873ba3b64a577ba0ccc857f
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-14 19:14:11 +0200

    formatting

--- MySQL/12. Exercise_Subqueries and JOINs/ExerciseSubqueriesAndJOINs/SubqueriesAndJOINs_Exercise.sql
+++ MySQL/12. Exercise_Subqueries and JOINs/ExerciseSubqueriesAndJOINs/SubqueriesAndJOINs_Exercise.sql
@@ -2,6 +2,6 @@
 USE soft_uni;
 SELECT employees.employee_id, employees.job_title, addresses.address_id, addresses.address_text
 FROM employees
-JOIN addresses ON employees.address_id = addresses.address_id
+         JOIN addresses ON employees.address_id = addresses.address_id
 ORDER BY address_id ASC
 LIMIT 5;
