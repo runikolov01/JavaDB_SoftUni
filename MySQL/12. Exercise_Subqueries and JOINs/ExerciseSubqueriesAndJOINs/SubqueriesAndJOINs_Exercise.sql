c2d41397c230e4b3f6c42947e07d87e3a2615364
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-14 19:28:55 +0200

    3. Sales Employee

--- MySQL/12. Exercise_Subqueries and JOINs/ExerciseSubqueriesAndJOINs/SubqueriesAndJOINs_Exercise.sql
+++ MySQL/12. Exercise_Subqueries and JOINs/ExerciseSubqueriesAndJOINs/SubqueriesAndJOINs_Exercise.sql
@@ -13,3 +13,11 @@ FROM employees AS e
          JOIN towns AS t ON a.town_id = t.town_id
 ORDER BY e.first_name, e.last_name
 LIMIT 5;
+
+-- 3. Sales Employee
+SELECT e.employee_id, e.first_name, e.last_name, d.name AS 'department_ name'
+FROM employees AS e
+         JOIN departments AS d
+              ON e.department_id = d.department_id
+WHERE d.name = 'Sales'
+ORDER BY employee_id DESC;
