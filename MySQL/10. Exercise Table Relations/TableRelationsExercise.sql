a0bae124bad65d15c9c0408b415436ae44ee9ab9
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-14 13:06:34 +0200

    04. Self-Referencing

--- MySQL/10. Exercise Table Relations/TableRelationsExercise.sql
+++ MySQL/10. Exercise Table Relations/TableRelationsExercise.sql
@@ -100,3 +100,19 @@ SELECT
     *
 FROM
     students_exams;
+    
+
+-- 04. Self-Referencing
+CREATE TABLE teachers (
+    teacher_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(15),
+    manager_id INT
+);
+INSERT INTO teachers(teacher_id,name,manager_id)
+VALUES(101,'John',NULL),(102,'Maya',106),(103,'Silvia',106),
+(104,'Ted',105),(105,'Mark',101),(106,'Greta',101);
+ALTER TABLE teachers
+ADD CONSTRAINT fk_manager_id FOREIGN KEY(manager_id)
+REFERENCES teachers(teacher_id)
+ON DELETE NO ACTION;
+SET FOREIGN_KEY_CHECKS=0;
