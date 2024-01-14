07f6061d0039a87375e0819a40a1684a6f4b46c2
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-14 19:09:37 +0200

    01. Employee Address

---
+++ MySQL/12. Exercise_Subqueries and JOINs/ExerciseSubqueriesAndJOINs/SubqueriesAndJOINs_Exercise.sql
@@ -0,0 +1,7 @@
+-- 01. Employee Address
+USE soft_uni;
+SELECT employees.employee_id, employees.job_title, addresses.address_id, addresses.address_text
+FROM employees
+JOIN addresses ON employees.address_id = addresses.address_id
+ORDER BY address_id ASC
+LIMIT 5;
