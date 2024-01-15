9e0ca1ed81bec806c77a80c0f327a7a273389958
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-15 11:36:06 +0200

    08. Employee 24

--- MySQL/12. Exercise_Subqueries and JOINs/ExerciseSubqueriesAndJOINs/SubqueriesAndJOINs_Exercise.sql
+++ MySQL/12. Exercise_Subqueries and JOINs/ExerciseSubqueriesAndJOINs/SubqueriesAndJOINs_Exercise.sql
@@ -65,3 +65,14 @@ WHERE DATE(p.start_date) > '2002-08-13'
   AND p.end_date IS NULL
 ORDER BY e.first_name, p.name
 LIMIT 5;
+
+-- 08. Employee 24
+SELECT e.employee_id,
+       e.first_name,
+       IF(YEAR(p.start_date) >= 2005, NULL, p.name) AS 'project_name'
+FROM employees AS e
+         JOIN employees_projects AS ep ON e.employee_id = ep.employee_id
+         JOIN projects AS p ON ep.project_id = p.project_id
+WHERE e.employee_id = 24
+ORDER BY e.first_name, p.name
+LIMIT 5;
