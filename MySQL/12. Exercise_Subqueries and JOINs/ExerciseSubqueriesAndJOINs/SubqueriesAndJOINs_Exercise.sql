0c06c57c041b803010818e2377b399c80cf67933
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-15 22:29:18 +0200

    15. Continents and Currencies

--- MySQL/12. Exercise_Subqueries and JOINs/ExerciseSubqueriesAndJOINs/SubqueriesAndJOINs_Exercise.sql
+++ MySQL/12. Exercise_Subqueries and JOINs/ExerciseSubqueriesAndJOINs/SubqueriesAndJOINs_Exercise.sql
@@ -137,3 +137,16 @@ FROM countries
 where continent_code = 'AF'
 ORDER BY countries.country_name ASC
 LIMIT 5;
+
+-- 15. *Continents and Currencies
+SELECT c.continent_code, currency_code, COUNT(*) AS 'currency_usage'
+FROM countries AS c
+GROUP BY c.continent_code, currency_code, c.currency_code
+HAVING currency_usage > 1
+   AND currency_usage = (SELECT COUNT(*) AS count_of_currencies
+                         FROM countries AS c2
+                         WHERE c2.continent_code = c.continent_code
+                         GROUP BY c2.currency_code
+                         ORDER BY count_of_currencies DESC
+                         LIMIT 1)
+ORDER BY c.continent_code, c.currency_code;
