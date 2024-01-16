0dea77871450b7b3c32a9534b5f78357a9a83864
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-16 21:44:21 +0200

    3. Employees Promotion By ID

--- MySQL/13. Database Programmability and Transactions/DatabaseProgrammabilityAndTransactions/DatabaseProgrammabilityAndTransactions.sql
+++ MySQL/13. Database Programmability and Transactions/DatabaseProgrammabilityAndTransactions/DatabaseProgrammabilityAndTransactions.sql
@@ -26,3 +26,14 @@ BEGIN
                            WHERE `name` = department_name);
 END$$
 DELIMITER ;
+
+-- 3. Employees Promotion By ID
+CREATE PROCEDURE usp_raise_salary_by_id(id INT)
+BEGIN
+    DECLARE employee_id_count INT;
+    SET employee_id_count := (SELECT COUNT(*) FROM employees WHERE employee_id = id);
+    IF (employee_id_count = 1)
+    THEN
+        UPDATE employees SET salary = salary * 1.05 WHERE employee_id = id;
+    END IF;
+END;
