632f787fd5f3e5bc803fe4a13644ddb744358282
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-16 21:49:34 +0200

    4. Triggered

--- MySQL/13. Database Programmability and Transactions/DatabaseProgrammabilityAndTransactions/DatabaseProgrammabilityAndTransactions.sql
+++ MySQL/13. Database Programmability and Transactions/DatabaseProgrammabilityAndTransactions/DatabaseProgrammabilityAndTransactions.sql
@@ -37,3 +37,30 @@ BEGIN
         UPDATE employees SET salary = salary * 1.05 WHERE employee_id = id;
     END IF;
 END;
+
+-- 4. Triggered
+CREATE TABLE deleted_employees
+(
+    employee_id   INT AUTO_INCREMENT PRIMARY KEY,
+    first_name    VARCHAR(50),
+    last_name     VARCHAR(50),
+    middle_name   VARCHAR(50),
+    job_title     VARCHAR(50),
+    department_id INT,
+    salary        DECIMAL(19, 4)
+);
+
+DELIMITER $$
+CREATE TRIGGER tr_after_delete_employees
+    AFTER DELETE
+    ON employees
+    FOR EACH ROW
+BEGIN
+    INSERT INTO deleted_employees (first_name, last_name, middle_name, job_title, department_id, salary)
+    VALUES (OLD.first_name,
+            OLD.last_name,
+            OLD.middle_name,
+            OLD.job_title,
+            OLD.department_id,
+            OLD.salary);
+END
