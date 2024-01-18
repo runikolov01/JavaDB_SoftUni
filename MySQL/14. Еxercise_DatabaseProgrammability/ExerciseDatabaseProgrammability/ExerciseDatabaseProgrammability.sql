6507eec123cab2ccc808436b74c565e5474176df
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-18 19:52:54 +0200

    01. Employees with Salary Above 35000

--- MySQL/14. Еxercise_DatabaseProgrammability/ExerciseDatabaseProgrammability/ExerciseDatabaseProgrammability.sql
+++ MySQL/14. Еxercise_DatabaseProgrammability/ExerciseDatabaseProgrammability/ExerciseDatabaseProgrammability.sql
@@ -1,1 +1,10 @@
 USE soft_uni;
+
+-- 01. Employees with Salary Above 35000
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_salary_above_35000()
+    BEGIN
+SELECT first_name, last_name FROM employees
+WHERE salary > 35000
+ORDER BY first_name ASC, last_name ASC, employee_id ASC;
+END$$
