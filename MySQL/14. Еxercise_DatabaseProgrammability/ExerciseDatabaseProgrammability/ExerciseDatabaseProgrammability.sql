5d1e9b5a224ce291b265f9d1726de1ad7bef67d4
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-18 20:22:56 +0200

    04. Employees from Town

--- MySQL/14. Еxercise_DatabaseProgrammability/ExerciseDatabaseProgrammability/ExerciseDatabaseProgrammability.sql
+++ MySQL/14. Еxercise_DatabaseProgrammability/ExerciseDatabaseProgrammability/ExerciseDatabaseProgrammability.sql
@@ -34,3 +34,17 @@ BEGIN
     WHERE name LIKE CONCAT(string, '%')
     ORDER BY name ASC;
 END$$
+DELIMITER ;
+
+-- 4. Employees from Town
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_from_town(town VARCHAR(50))
+BEGIN
+    SELECT employees.first_name, employees.last_name
+    FROM employees
+             JOIN addresses ON employees.address_id = addresses.address_id
+             JOIN towns ON addresses.town_id = towns.town_id
+    WHERE towns.name = town
+    ORDER BY first_name ASC, last_name ASC, employee_id ASC;
+END$$
+DELIMITER ;
