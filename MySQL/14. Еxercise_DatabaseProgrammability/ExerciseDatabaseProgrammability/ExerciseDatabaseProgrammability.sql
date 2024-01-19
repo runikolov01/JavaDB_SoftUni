a54604b249041d8e164cabd0cd6ad078dc9c2eda
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-19 19:44:38 +0200

    15. Log Accounts Trigger

--- MySQL/14. Еxercise_DatabaseProgrammability/ExerciseDatabaseProgrammability/ExerciseDatabaseProgrammability.sql
+++ MySQL/14. Еxercise_DatabaseProgrammability/ExerciseDatabaseProgrammability/ExerciseDatabaseProgrammability.sql
@@ -173,3 +173,24 @@ BEGIN
     END IF;
 END $$
 DELIMITER ;
+
+-- 15. Log Accounts Trigger
+CREATE TABLE logs
+(
+    log_id     INT PRIMARY KEY AUTO_INCREMENT,
+    account_id INT NOT NULL,
+    old_sum    DECIMAL(19, 4),
+    new_sum    DECIMAL(19, 4)
+);
+
+CREATE TRIGGER trigger_balance_update
+    AFTER UPDATE
+    ON accounts
+    FOR EACH ROW
+BEGIN
+    IF OLD.balance <> NEW.balance
+    THEN
+        INSERT INTO logs(account_id, old_sum, new_sum)
+            VALUE (OLD.id, OLD.balance, NEW.balance);
+    END IF;
+END;
