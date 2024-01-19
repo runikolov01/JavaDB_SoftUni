a1e1f76cb1a4abaaac68758e1cadf7ce93fe26a9
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-19 19:34:05 +0200

    13. Withdraw Money

--- MySQL/14. Еxercise_DatabaseProgrammability/ExerciseDatabaseProgrammability/ExerciseDatabaseProgrammability.sql
+++ MySQL/14. Еxercise_DatabaseProgrammability/ExerciseDatabaseProgrammability/ExerciseDatabaseProgrammability.sql
@@ -129,3 +129,25 @@ BEGIN
     WHERE a.id = account_id;
 END$$
 DELIMITER ;
+
+-- 13. Withdraw Money
+DELIMITER $$
+CREATE PROCEDURE usp_withdraw_money(account_id INT, money_amount DECIMAL(19, 4))
+BEGIN
+    IF money_amount > 0
+    THEN
+        START TRANSACTION ;
+
+        UPDATE accounts AS a
+        SET a.balance = a.balance - money_amount
+        WHERE account_id = a.id;
+
+        IF (SELECT balance FROM accounts WHERE account_id = id) < 0
+        THEN
+            ROLLBACK ;
+        ELSE
+            COMMIT ;
+        END IF;
+    END IF;
+END $$
+DELIMITER ;
