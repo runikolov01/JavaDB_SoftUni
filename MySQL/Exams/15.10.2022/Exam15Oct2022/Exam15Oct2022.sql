b34c61eae0086a65b631860389a275db95a42eb4
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-19 21:44:17 +0200

    06. Birthdate

--- MySQL/Exams/15.10.2022/Exam15Oct2022/Exam15Oct2022.sql
+++ MySQL/Exams/15.10.2022/Exam15Oct2022/Exam15Oct2022.sql
@@ -96,5 +96,14 @@ FROM waiters AS w
 WHERE (SELECT COUNT(*) FROM orders WHERE waiter_id = w.id) = 0;
 
 -- 05. Clients
-SELECT * FROM clients
+SELECT *
+FROM clients
 ORDER BY birthdate DESC, id DESC;
+
+-- 06. Birthdate
+SELECT first_name, last_name, birthdate, review
+FROM clients
+WHERE card IS NULL
+  AND YEAR(birthdate) BETWEEN 1978 AND 1993
+ORDER BY last_name DESC, id ASC
+LIMIT 5;
