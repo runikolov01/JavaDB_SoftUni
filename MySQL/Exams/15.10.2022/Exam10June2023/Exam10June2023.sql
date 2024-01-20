03821a736b7e9f834f68a9fdc96366ba3a470669
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-20 11:41:39 +0200

    07. New students

--- MySQL/Exams/15.10.2022/Exam10June2023/Exam10June2023.sql
+++ MySQL/Exams/15.10.2022/Exam10June2023/Exam10June2023.sql
@@ -108,3 +108,13 @@ FROM students
 WHERE age >= 21
 ORDER BY first_name DESC, email ASC, id ASC
 LIMIT 10;
+
+-- 07. New students
+SELECT
+    CONCAT(s.first_name,' ', s.last_name) AS full_name,
+    SUBSTRING(email, 2, 10) AS username,
+    reverse(phone) AS password
+FROM students s
+         LEFT JOIN students_courses sc ON s.id = sc.student_id
+WHERE sc.student_id IS NULL
+ORDER BY password DESC;
