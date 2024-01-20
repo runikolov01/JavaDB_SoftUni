2c1db5bf41639dac029425adcba25c27c4ab08e1
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-20 20:35:55 +0200

    02. Insert

--- MySQL/Exams/11.12.2022/Exam11Dec2022/Exam11Dec2022.sql
+++ MySQL/Exams/11.12.2022/Exam11Dec2022/Exam11Dec2022.sql
@@ -58,3 +58,15 @@ CREATE TABLE flights_passengers
     CONSTRAINT FOREIGN KEY (passenger_id)
         REFERENCES passengers (id)
 );
+
+-- 02. Insert
+INSERT INTO airplanes (model, passengers_capacity, tank_capacity, cost)
+SELECT
+    CONCAT(REVERSE(first_name), '797') AS model,
+    (LENGTH(last_name) * 17) AS passengers_capacity,
+    (id * 790) AS tank_capacity,
+    (LENGTH(first_name) * 50.6) AS cost
+FROM passengers
+WHERE id <= 5;
+
+-- 03. Update
