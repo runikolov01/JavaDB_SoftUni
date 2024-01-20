62e1671f27563544b3cc0c9f1ad40c5b3d07a2ac
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-20 20:25:04 +0200

    01. Table Design

---
+++ MySQL/Exams/11.12.2022/Exam11Dec2022/Exam11Dec2022.sql
@@ -0,0 +1,60 @@
+-- 01. Table Design
+CREATE DATABASE airlines;
+USE airlines;
+-- DROP DATABASE airlines;
+
+CREATE TABLE countries
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(30) NOT NULL UNIQUE,
+    description TEXT,
+    currency    VARCHAR(5)  NOT NULL
+);
+
+CREATE TABLE airplanes
+(
+    id                  INT PRIMARY KEY AUTO_INCREMENT,
+    model               VARCHAR(50)    NOT NULL UNIQUE,
+    passengers_capacity INT            NOT NULL,
+    tank_capacity       DECIMAL(19, 2) NOT NULL,
+    cost                DECIMAL(19, 2) NOT NULL
+);
+
+CREATE TABLE passengers
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name  VARCHAR(30) NOT NULL,
+    country_id INT         NOT NULL,
+    CONSTRAINT fk_passengers_countries
+        FOREIGN KEY (country_id)
+            REFERENCES countries (id)
+);
+
+CREATE TABLE flights
+(
+    id                  INT PRIMARY KEY AUTO_INCREMENT,
+    flight_code         VARCHAR(30) NOT NULL UNIQUE,
+    departure_country   INT         NOT NULL,
+    CONSTRAINT fk_flight_country
+        FOREIGN KEY (departure_country)
+            REFERENCES countries (id),
+    destination_country INT         NOT NULL,
+    CONSTRAINT fk_flight_destination FOREIGN KEY (destination_country)
+        REFERENCES countries (id),
+    airplane_id         INT         NOT NULL,
+    CONSTRAINT fk_flight_airplane FOREIGN KEY (airplane_id)
+        REFERENCES airplanes (id),
+    has_delay           TINYINT(1),
+    departure           DATETIME
+);
+
+CREATE TABLE flights_passengers
+(
+    flight_id    INT,
+    CONSTRAINT FOREIGN KEY (flight_id)
+        REFERENCES flights (id),
+    passenger_id INT,
+    CONSTRAINT FOREIGN KEY (passenger_id)
+        REFERENCES passengers (id)
+);
