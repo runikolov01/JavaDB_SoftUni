93ce307f2577340b15604104cf8d66a53f10ec28
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-21 10:34:28 +0200

    02. Insert

--- MySQL/Exams/06.08.2023/Exam06Aug2023/Exam06Aug2023.sql
+++ MySQL/Exams/06.08.2023/Exam06Aug2023/Exam06Aug2023.sql
@@ -88,3 +88,15 @@ CREATE TABLE property_transactions
     iban             VARCHAR(40) UNIQUE,
     is_successful    TINYINT(1)
 );
+
+-- 02. Insert
+
+INSERT INTO property_transactions(property_id, buyer_id, transaction_date, bank_name, iban, is_successful)
+    (SELECT (agent_id + DAY(offer_datetime)),
+            (agent_id + MONTH(offer_datetime)),
+            DATE(offer_datetime),
+            CONCAT('Bank ', agent_id),
+            CONCAT('BG', price, agent_id),
+            (1)
+     FROM property_offers
+     WHERE agent_id <= 2);
