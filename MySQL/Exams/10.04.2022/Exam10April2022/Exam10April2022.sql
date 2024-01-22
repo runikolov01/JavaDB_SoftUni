535399bd6b58d362f0bba15d24fa4a4bc040e3ba
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-22 19:16:13 +0200

    02. Insert

--- MySQL/Exams/10.04.2022/Exam10April2022/Exam10April2022.sql
+++ MySQL/Exams/10.04.2022/Exam10April2022/Exam10April2022.sql
@@ -81,3 +81,14 @@ CREATE TABLE genres_movies
         FOREIGN KEY (movie_id)
             REFERENCES movies (id)
 );
+
+-- 02. Insert
+INSERT INTO actors(first_name, last_name, birthdate, height, awards, country_id)
+    (SELECT REVERSE(first_name),
+            REVERSE(last_name),
+            DATE_SUB(birthdate, INTERVAL 2 DAY),
+            (height + 10),
+            country_id,
+            (3)
+     FROM actors
+     WHERE actors.id <= 10);
