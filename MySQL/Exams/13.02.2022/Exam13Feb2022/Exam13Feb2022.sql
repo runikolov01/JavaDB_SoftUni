b49ccc123085aa9cb7ae19b139d667d613db0bba
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-23 22:49:59 +0200

    08. First customers

--- MySQL/Exams/13.02.2022/Exam13Feb2022/Exam13Feb2022.sql
+++ MySQL/Exams/13.02.2022/Exam13Feb2022/Exam13Feb2022.sql
@@ -116,3 +116,12 @@ FROM reviews
 WHERE content LIKE 'My%'
   AND LENGTH(content) > 61
 ORDER BY rating DESC;
+
+-- 08. First customers
+SELECT CONCAT_WS(' ', first_name, last_name) AS full_name,
+       address,
+       order_datetime AS order_date
+FROM customers
+         JOIN orders o on customers.id = o.customer_id
+WHERE YEAR(order_datetime) <= 2018
+ORDER BY full_name DESC;
