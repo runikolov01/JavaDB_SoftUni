5574fd3534c8d4fa2614741d074aa1039cc706b4
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-23 22:45:49 +0200

    06. Quantity

--- MySQL/Exams/13.02.2022/Exam13Feb2022/Exam13Feb2022.sql
+++ MySQL/Exams/13.02.2022/Exam13Feb2022/Exam13Feb2022.sql
@@ -99,5 +99,13 @@ FROM customers
 WHERE customer_id IS NULL;
 
 -- 05. Categories
-SELECT * FROM categories
+SELECT *
+FROM categories
 ORDER BY name DESC;
+
+-- 06. Quantity
+SELECT id, brand_id, name, quantity_in_stock
+FROM products
+WHERE price > 1000
+  AND quantity_in_stock < 30
+ORDER BY quantity_in_stock, id;
