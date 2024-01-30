e374ee0ec282e7d712138e98e6cbfb2c37fd2b22
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-30 23:04:33 +0200

    Exercise: Introduction to Hibernate

---
+++ SpringData/6. Exercise_Introduction to Hibernate/intro-to-hibernate/src/main/java/Main.java
@@ -0,0 +1,30 @@
+import entities.Town;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("soft_uni");
+        final EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+
+        entityManager.getTransaction().begin();
+
+        List<Town> allTowns = entityManager.createQuery("FROM Town", Town.class).getResultList();
+
+
+        for (Town town : allTowns) {
+            if (town.getName().length() > 5) {
+                entityManager.detach(town);
+                continue;
+            }
+            town.setName(town.getName().toUpperCase());
+            entityManager.persist(town);
+        }
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+}
