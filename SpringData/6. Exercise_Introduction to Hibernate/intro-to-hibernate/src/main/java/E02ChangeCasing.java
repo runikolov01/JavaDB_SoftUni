5f9869636901a019b5de7fe689e69dbb06b8045b
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-30 23:05:28 +0200

    2. Change casing

--- SpringData/6. Exercise_Introduction to Hibernate/intro-to-hibernate/src/main/java/E02ChangeCasing.java
+++ SpringData/6. Exercise_Introduction to Hibernate/intro-to-hibernate/src/main/java/E02ChangeCasing.java
@@ -5,7 +5,8 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.List;
 
-public class Main {
+public class E02ChangeCasing
+ {
     public static void main(String[] args) {
         final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("soft_uni");
         final EntityManager entityManager = entityManagerFactory.createEntityManager();
