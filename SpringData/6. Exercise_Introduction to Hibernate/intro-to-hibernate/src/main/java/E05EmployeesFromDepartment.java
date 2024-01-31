271b912b2127ea47bde618c37f10d2546db6b5e0
Rumen Nikolov <rumenhco2001@abv.bg>
2024-01-31 18:42:08 +0200

    5. Employees from Department

--- SpringData/6. Exercise_Introduction to Hibernate/intro-to-hibernate/src/main/java/E05EmployeesFromDepartment.java
+++ SpringData/6. Exercise_Introduction to Hibernate/intro-to-hibernate/src/main/java/E05EmployeesFromDepartment.java
@@ -6,7 +6,7 @@ public class E05EmployeesFromDepartment {
     private static final String PRINT_FORMAT = "%s %s from %s - $%f.02";
 
     public static void main(String[] args) {
-Connector.createEntityManager()
+        Connector.createEntityManager()
                 .createQuery("FROM Employee WHERE department.name = :dName ORDER BY salary, id", Employee.class)
                 .setParameter("dName", "Research and Development")
                 .getResultList()
